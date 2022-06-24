d293509c4b39a162f945831558427adefda35e4c
Naefunn <nathan.d.foster1@gmail.com>
2022-06-24 12:59:41 +0100

    first 3 tests complete

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,14 @@
-# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(name):
+    return name["name"]
+
+
+def get_total_cash(total):
+    return total["admin"]["total_cash"]
+
+
+def add_or_remove_cash(total_cash, add_cash):
+    total_cash["admin"]["total_cash"] += add_cash
+    return total_cash
+
+
+
