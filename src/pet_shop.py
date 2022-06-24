f98b54deee61ddcf6fc61f9de4e0a49da16f8f4d
Naefunn <nathan.d.foster1@gmail.com>
2022-06-24 15:12:15 +0100

    finished all but one compulsory test

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,3 +1,6 @@
+from xml.dom.minidom import Element
+
+
 def get_pet_shop_name(name):
     return name["name"]
 
@@ -16,6 +19,55 @@ def get_pets_sold(dict):
 def increase_pets_sold(dict, new_pet):
     dict["admin"]["pets_sold"]+= new_pet
     return dict
+
+def get_stock_count(dict):
+    stock_num = len(dict["pets"])
+    return stock_num
+
+def get_pets_by_breed(dict, name):
+    pets_with_name = []
+    for item in dict["pets"]:
+        if item["breed"] == name:
+            pets_with_name.append(item)
+    return pets_with_name
+    
+def find_pet_by_name(dict, name):
+    for item in dict["pets"]:
+        if item["name"] ==  name:
+            return item
+
+def remove_pet_by_name(dict, name):
+    index = -1
+    for item in dict["pets"]:
+        index += 1
+        if item["name"] == name:
+            dict["pets"].pop(index)
+    return dict
+
+def add_pet_to_stock(dict, new_pet):
+    dict["pets"].append(new_pet)
+    return dict
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, cash):
+    customer["cash"] -= cash
+    return customer["cash"]
+
+
+def get_customer_pet_count(customer):
+    pet_count = 0
+    pet_count += len(customer["pets"])
+    return pet_count
+
+# def add_pet_to_customer():
+    
+
+
+
+
+
 
 
 
