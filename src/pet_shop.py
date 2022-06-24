c40363a279abc258b55053f63a5703b750812bad
Naefunn <nathan.d.foster1@gmail.com>
2022-06-24 13:18:29 +0100

    6 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -10,5 +10,16 @@ def add_or_remove_cash(total_cash, add_cash):
     total_cash["admin"]["total_cash"] += add_cash
     return total_cash
 
+def get_pets_sold(dict):
+    return dict["admin"]["pets_sold"]
+
+def increase_pets_sold(dict, new_pet):
+    dict["admin"]["pets_sold"]+= new_pet
+    return dict
+
+
+
+
+
 
 
